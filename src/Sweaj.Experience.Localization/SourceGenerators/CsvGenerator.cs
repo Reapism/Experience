21a4910c18329628bdea7a3def23adcbe208d473
Anthony J <reapsprgm@gmail.com>
2022-09-26 22:43:31 -0400

    Add CSVs for localization of text

---
+++ src/Sweaj.Experience.Localization/SourceGenerators/CsvGenerator.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.CodeAnalysis;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sweaj.Experience.Localization.SourceGenerators
+{
+    internal sealed class CsvGenerator : ISourceGenerator
+    {
+        public void Initialize(GeneratorInitializationContext context)
+        {
+            
+        }
+
+        public void Execute(GeneratorExecutionContext context)
+        {
+            context.
+        }
+    }
+}
